1808f7fab316f9ed934cee7113a87930f8018fd0
unknown <serranojessica05@gmail.com>
2019-07-10 09:36:59 -0700

    this is my first commit!

---
+++ home.js
@@ -0,0 +1,17 @@
+function alert_name() {
+  //Create name variable
+  var name = "Jessica ";
+  alert("My name is "+ name);
+  console.log("Test 123");
+}
+
+function change_image() {
+  var image = document.getElementById('shawn1_image');
+  image.src = "img/shawn2.jpg";
+}
+
+function increase_size() {
+  var image= document.getElementById('shawn1_image');
+  image.style.width= "600px"
+
+}
